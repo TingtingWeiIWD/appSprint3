466716cffcba9e704dd5531260521c3fc2f6354b
quinnienguyen1105 <quinnienguyen1105@gmail.com>
2023-04-06 14:11:03 -0400

    Updated code to show tasks list

--- src/App.js
+++ src/App.js
@@ -14,19 +14,22 @@ import HelpRemovePage from "./pages/HelpPage/HelpRemovePage";
 import HelpStatusPage from "./pages/HelpPage/HelpStatusPage";
 import NotFoundPage from "./pages/NotFoundPage";
 import { useEffect } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "./database/config";
 function App() {
   const dispatch = useDispatch();
-
+  const tasksCollectionRef = collection(db, 'tasks');
+  
   //Get the tasks from database onload
   useEffect(() => {
     (async () => {
-      const tasks = await database.loadTasks();
+      const tasks = await getDocs(tasksCollectionRef);
       const sortedTasks = tasks.docs.map((doc) => ({ ...doc.data(), id: doc.id })).sort((b, a) => {
       return a.createdAt - b.createdAt;
       });
       dispatch(setTask(sortedTasks));
     })();
-  }, []);
+  }, [tasksCollectionRef]);
 
   return (
     <>
